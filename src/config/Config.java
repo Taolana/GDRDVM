39130b48a21293367f08a6ac64bb327e227bea1a
Niaintsoa <34262183+Taolana@users.noreply.github.com>
2020-10-27 11:02:38 +0300

    Update Config

--- src/config/Config.java
+++ src/config/Config.java
@@ -12,7 +12,7 @@ package config;
 public class Config {
 
     public static final String driver = "com.mysql.jdbc.Driver";//Ex : "oracle.jdbc.OracleDriver"
-    public static final String url = "jdbc:mysql://localhost/gdrm";//Ex : "jdbc:oracle:thin:@192.168.200.91:1521:DATA10G";
-    public static final String user = "bynan_dev";
-    public static final String password = "bynan_dev_1234";
+    public static final String url = "jdbc:mysql://localhost/";//Ex : "jdbc:oracle:thin:@192.168.200.91:1521:DATA10G";
+    public static final String user = "";
+    public static final String password = "";
 }
